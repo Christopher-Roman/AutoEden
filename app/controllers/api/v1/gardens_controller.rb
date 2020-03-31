a0349db3086c9e95ec84f1b0a6cfa6fcb67c8489
Chris Roman <Christopherroman85@gmail.com>
2020-03-31 16:50:04 -0500

    gardens show route is working

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -11,6 +11,23 @@ module Api
 				}, status: :ok
 			end
 
+			def show
+				garden = Garden.find(params[:id])
+				if garden
+					render json: {
+						status: 'Success',
+						message: 'Here is the requested garden.',
+						data: garden
+					}, status: :ok
+				else
+					render json: {
+						status: 'Something went wrong.',
+						message: 'See error message to find out what happened.',
+						data: garden.errors
+					}, status: :ok
+				end
+			end
+
 		end
 	end
 end
