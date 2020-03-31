d0d7aed7c62acb36bbb497e9e18cf4bf1fdec139
Chris Roman <Christopherroman85@gmail.com>
2020-03-31 11:48:06 -0500

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace 'api' do
+
+  	namespace 'v1' do
+
+  		resources :users
+  		resources :gardens
+  		resources :journals
+  		resources :plants
+  		resources :entries
+
+  	end
+
+  end
+  
 end
